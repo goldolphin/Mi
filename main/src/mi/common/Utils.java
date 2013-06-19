304cc2f09ff4b8fe661e25aa11a93010c6307a55
goldolphin <goldolphin@gmail.com>
2013-06-20 00:30:42 +0800

    ADD: CharHashMap

---
+++ main/src/mi/common/Utils.java
@@ -0,0 +1,14 @@
+package mi.common;
+
+/**
+ * User: goldolphin
+ * Time: 2013-06-15 06:47
+ */
+public final class Utils {
+    public static void performance(Runnable runnable) {
+        long start = System.currentTimeMillis();
+        runnable.run();
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println("Time elapsed: " + elapsed + "ms");
+    }
+}
